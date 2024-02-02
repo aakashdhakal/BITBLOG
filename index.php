6597c9189048900b7da1aaf87397bcb7af0572d1
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-02-02 12:41:28 +0545

    Refactor routing logic and remove unused code

--- index.php
+++ index.php
@@ -1,111 +1,33 @@
 <?php
 
-function get($route, $path_to_include)
-{
-    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        route($route, $path_to_include);
-    }
-}
-function post($route, $path_to_include)
-{
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        route($route, $path_to_include);
-    }
-}
-function put($route, $path_to_include)
-{
-    if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
-        route($route, $path_to_include);
-    }
-}
-function patch($route, $path_to_include)
-{
-    if ($_SERVER['REQUEST_METHOD'] == 'PATCH') {
-        route($route, $path_to_include);
-    }
-}
-function delete($route, $path_to_include)
-{
-    if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-        route($route, $path_to_include);
-    }
-}
-function any($route, $path_to_include)
-{
-    route($route, $path_to_include);
-}
-function route($route, $path_to_include)
-{
-    $callback = $path_to_include;
-    if (!is_callable($callback)) {
-        if (!strpos($path_to_include, '.php')) {
-            $path_to_include .= '.php';
-        }
-    }
-    if ($route == "/404") {
-        include_once __DIR__ . "/$path_to_include";
-        exit();
-    }
-    $request_url = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
-    $request_url = rtrim($request_url, '/');
-    $request_url = strtok($request_url, '?');
-    $route_parts = explode('/', $route);
-    $request_url_parts = explode('/', $request_url);
-    array_shift($route_parts);
-    array_shift($request_url_parts);
-    if ($route_parts[0] == '' && count($request_url_parts) == 0) {
-        // Callback function
-        if (is_callable($callback)) {
-            call_user_func_array($callback, []);
-            exit();
-        }
-        include_once __DIR__ . "/$path_to_include";
-        exit();
-    }
-    if (count($route_parts) != count($request_url_parts)) {
-        return;
-    }
-    $parameters = [];
-    for ($__i__ = 0; $__i__ < count($route_parts); $__i__++) {
-        $route_part = $route_parts[$__i__];
-        if (preg_match("/^[$]/", $route_part)) {
-            $route_part = ltrim($route_part, '$');
-            array_push($parameters, $request_url_parts[$__i__]);
-            $$route_part = $request_url_parts[$__i__];
-        } else if ($route_parts[$__i__] != $request_url_parts[$__i__]) {
-            return;
-        }
-    }
-    // Callback function
-    if (is_callable($callback)) {
-        call_user_func_array($callback, $parameters);
-        exit();
-    }
-    include_once __DIR__ . "/$path_to_include";
-    exit();
-}
-function out($text)
-{
-    echo htmlspecialchars($text);
-}
+require_once __DIR__ . '/router.php';
 
-function set_csrf()
-{
-    session_start();
-    if (!isset($_SESSION["csrf"])) {
-        $_SESSION["csrf"] = bin2hex(random_bytes(50));
-    }
-    echo '<input type="hidden" name="csrf" value="' . $_SESSION["csrf"] . '">';
-}
+// ##################################################
+// ##################################################
+// ##################################################
 
-function is_csrf_valid()
-{
-    session_start();
-    if (!isset($_SESSION['csrf']) || !isset($_POST['csrf'])) {
-        return false;
-    }
-    if ($_SESSION['csrf'] != $_POST['csrf']) {
-        return false;
-    }
-    return true;
-}
+// Static GET
+// In the URL -> http://localhost
+// The output -> Index
+get('/dashboard', 'UI/dashboard/dashboard.php');
+get('', 'UI/homepage/home.php');
+get('/home', 'UI/homepage/home.php');
+get('/post/$slug_url', 'UI/post/blog-post.php');
+get('/logout', 'config/logout.php');
+get('/$i', 'UI/homepage/home.php');
+get('/api/postAuthors', 'api/postAuthors.php');
+
+post('/bookmark-config', 'config/bookmark-config.php');
+post('/post-like-config', 'config/post-like-config.php');
+post('/follow-config', 'config/follow-config.php');
+post('/post-list-config', 'config/post-list-config.php');
+post('/otp-config', 'config/otp-config.php');
+post('/login-config', 'config/login-config.php');
+post('/signup-config', 'config/signup-config.php');
+post('/signup-validate-config', 'config/signup-validate-config.php');
+post('/feedback-config', 'config/feedback-config.php');
+
+// For GET or POST
+// The 404.php which is inside the views folder will be called
+// The 404.php has access to $_GET and $_POST
+any('/404', 'UI/404.php');
