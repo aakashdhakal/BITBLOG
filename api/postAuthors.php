3faa60f0f26b65840cf1c5367a88da7f59791160
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-02-02 12:33:54 +0545

    Refactor routes and file paths

---
+++ api/postAuthors.php
@@ -0,0 +1,47 @@
+<?php
+
+session_start();
+$conn = new mysqli("sql211.infinityfree.com", "if0_35339284", "u2GJR7Y0VE", "if0_35339284_blogs");
+
+class blog
+{
+    public $id;
+    public $title;
+    public $content;
+    public $author;
+    public $date;
+    public $image;
+    public $category;
+}
+
+if (isset($_GET['username'])) {
+    $username = $_GET['username'];
+    $sql = "SELECT * FROM blog WHERE author = ?";
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt === false) {
+        echo json_encode(array('message' => 'Database error'));
+        exit;
+    }
+
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $posts = array();
+
+    while ($row = $result->fetch_assoc()) {
+        $post = new blog();
+        $post->id = $row['id'];
+        $post->title = $row['title'];
+        $post->content = $row['content'];
+        $post->author = $row['author'];
+        $post->date = $row['date'];
+        $post->image = $row['image'];
+        $post->category = $row['category'];
+        array_push($posts, $post);
+    }
+
+    echo json_encode($posts);
+} else {
+    echo json_encode(array('message' => 'No post found'));
+}
--- routes.php
+++ routes.php
@@ -9,24 +9,25 @@ require_once __DIR__ . '/router.php';
 // Static GET
 // In the URL -> http://localhost
 // The output -> Index
-get('/A.D-Blogs/dashboard', 'UI/dashboard/dashboard.php');
-get('/A.D-Blogs', 'UI/homepage/home.php');
-get('/A.D-Blogs/home', 'UI/homepage/home.php');
-get('/A.D-Blogs/post/$slug_url', 'UI/post/blog-post.php');
-get('/A.D-Blogs/logout', 'config/logout.php');
-get('/A.D-Blogs/$i', 'UI/homepage/home.php');
+get('/dashboard', 'UI/dashboard/dashboard.php');
+get('', 'UI/homepage/home.php');
+get('/home', 'UI/homepage/home.php');
+get('/post/$slug_url', 'UI/post/blog-post.php');
+get('/logout', 'config/logout.php');
+get('/$i', 'UI/homepage/home.php');
+get('/api/postAuthors', 'api/postAuthors.php');
 
-post('/A.D-Blogs/bookmark-config', 'config/bookmark-config.php');
-post('/A.D-Blogs/post-like-config', 'config/post-like-config.php');
-post('/A.D-Blogs/follow-config', 'config/follow-config.php');
-post('/A.D-Blogs/post-list-config', 'config/post-list-config.php');
-post('/A.D-Blogs/otp-config', 'config/otp-config.php');
-post('/A.D-Blogs/login-config', 'config/login-config.php');
-post('/A.D-Blogs/signup-config', 'config/signup-config.php');
-post('/A.D-Blogs/signup-validate-config', 'config/signup-validate-config.php');
-post('/A.D-Blogs/feedback-config', 'config/feedback-config.php');
+post('/bookmark-config', 'config/bookmark-config.php');
+post('/post-like-config', 'config/post-like-config.php');
+post('/follow-config', 'config/follow-config.php');
+post('/post-list-config', 'config/post-list-config.php');
+post('/otp-config', 'config/otp-config.php');
+post('/login-config', 'config/login-config.php');
+post('/signup-config', 'config/signup-config.php');
+post('/signup-validate-config', 'config/signup-validate-config.php');
+post('/feedback-config', 'config/feedback-config.php');
 
 // For GET or POST
 // The 404.php which is inside the views folder will be called
 // The 404.php has access to $_GET and $_POST
-any('/A.D-Blogs/404', 'UI/404.php');
+any('/404', 'UI/404.php');
