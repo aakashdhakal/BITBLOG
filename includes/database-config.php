24e6cabc6a727b337183da46c824b4989ec44a32
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2024-05-23 11:54:03 +0545

    Update database-config.php

--- includes/database-config.php
+++ includes/database-config.php
@@ -1,4 +1,3 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "blogs");
-//$conn = new mysqli("sql211.infinityfree.com", "if0_35339284", "u2GJR7Y0VE", "if0_35339284_blogs");
-//$conn = new mysqli("localhost", "yqptjkjp_aakashdhakal", "JZOGX@sD?7c.", "yqptjkjp_blogs");
+
